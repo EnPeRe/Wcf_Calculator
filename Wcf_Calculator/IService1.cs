fde2c23991286f21da9eec3546ee2338de060429
E <epr1122@hotmail.com>
2018-05-13 21:02:20 +0200

    Agregar archivos de proyecto.

---
+++ Wcf_Calculator/IService1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Wcf_Calculator
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int Sum(int num1, int num2);
+
+        [OperationContract]
+        int Substract(int num1, int num2);
+
+        [OperationContract]
+        int Multiply(int num1, int num2);
+
+        [OperationContract]
+        int Divide(int num1, int num2);
+    }
+}
---
+++ Wcf_Calculator/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Wcf_Calculator
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class Service1 : IService1
+    {
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
